22c4d7c72390278c745ca063663f4b284867e0b3
fostercoryj <57597262+fostercoryj@users.noreply.github.com>
2020-04-17 10:59:43 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, request, redirect, flash, session
-from flask_bcrypt import Bcrypt
-from mysqlconnection import connectToMySQL
-import re
-configfiletest = '*******GONNA BE HARD TO MISS THIS*******'
